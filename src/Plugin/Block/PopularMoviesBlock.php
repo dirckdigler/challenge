963df280acb2cae7ca56449fd7299ef90c0ead00
Edwin Vega <edwin.vega.moreno@gmail.com>
2019-07-29 18:51:11 -0500

    block popular movies

--- src/Plugin/Block/PopularMoviesBlock.php
+++ src/Plugin/Block/PopularMoviesBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\challenge\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'PopularMoviesBlock' block.
@@ -18,10 +19,51 @@ class PopularMoviesBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['popular_movies_block']['#markup'] = 'Implement PopularMoviesBlock.';
+   $calling = \Drupal::service('challenge.apiary');
+    try {
+      $response = $calling->getQuery('movie/popular');
+      $build = [];
+      $item_number = \Drupal::config('challenge.settings')
+        ->get('popular_movies_number');
+      $item_filter = array_slice($response['results'], 0, $item_number);
+      $genre = $calling->getQuery('genre/movie/list');
+      foreach ($item_filter as $item) {
+        $build['#theme'] = 'block--upcommingblock';
+        $build['#results'][] = $item;
+        $build['#base_url'] = $calling->baseUrl;
+      }
+    } catch (\Exception $e) {
+      $build['error'] = $e->getMessage();
+      $build['code'] = $e->getCode();
+    }
 
     return $build;
+  }
+
+   /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = \Drupal::config('challenge.settings');
+    $range = range(1, 10);
+    $form['popular_movies_number'] = array(
+    '#type' => 'select',
+    '#title' => $this->t('Number of movies'),
+    '#default_value' => (NULL !== $config->get('popular_movies_number'))
+      ? $config->get('popular_movies_number') : 10,
+    '#options' => array_combine($range, $range),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $config = \Drupal::configFactory()->getEditable('challenge.settings');
+    $config->set('popular_movies_number', $form_state
+      ->getValue('popular_movies_number'))->save();
   }
 
 }
